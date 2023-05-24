5e81aa99ea315f36e81aa23aac0264340671f0bc
AndreD4 <andremai629@gmail.com>
2023-05-24 10:43:45 +0900

    printing path to console

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -18,21 +18,7 @@ public class EnemyMover : MonoBehaviour
         Debug.Log(wayPoint.name);
       }
     }
-   /*
-    LOGIC!!!!!
-    We're going to start our routine, which starts our for loop.
-    We're going to set up the start and end position we want to move to.
-    And then while our travel percent is less than one, so while we're not at our end position, this is
-    really saying we're going to update the travel percent with the Times, Delta time and then move the
-    position of our enemy.
-    We're then going to yield back to the update function until the end of the frame has been completed
-    and then we'll jump back to our routine, which will continue our while loop and go through our lines
-    here.
-    And this will carry on until our travel percent is greater than one, at which point our wall loop will
-    be broken out of, and then we'll go onto the next waypoint in our path.
-    That whole system will continue to loop until we've run out of waypoints to visit.
-  */
+   
 
-    
 
 }
