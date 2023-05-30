985c1847eda8d8ad0d984fa5a376e4c4fb34a4bf
AndreD4 <andremai629@gmail.com>
2023-05-30 14:12:24 +0900

    setting up tower to follow enemy

---
+++ Assets/Tower/TargetLocator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+    [SerializeField] Transform weapon;
+    [SerializeField] Transform target;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        AimWeapon();
+    }
+
+    void AimWeapon()
+    {
+      
+    }
+}
