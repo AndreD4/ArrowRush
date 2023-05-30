2cdee369aad33bdb8c9526a557b42e6add1c577f
AndreD4 <andremai629@gmail.com>
2023-05-30 12:55:09 +0900

    enemy following path

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
+
 {
   [SerializeField] List<WayPoint> path = new List<WayPoint>();
+  [SerializeField] float waitTime = 1f;
 
     void Start() 
     {
-      PrintWayPointName();
-      
+      StartCoroutine(FollowPath());
     }
 
-    IEnumerator PrintWayPointName() 
+    IEnumerator FollowPath() 
     {
       foreach(WayPoint wayPoint in path)
       {
-        Debug.Log(wayPoint.name);
-        yield return new WaitForSeconds(1f);
+        transform.position = wayPoint.transform.position;
+        yield return new WaitForSeconds(waitTime);
       }
     }
    
-
-
 }
