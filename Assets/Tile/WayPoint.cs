cc67682cb3056cad283c5a263b9f49ef4aee5593
AndreD4 <andremai629@gmail.com>
2023-05-30 14:04:56 +0900

    Instantiate gameobject

--- Assets/Tile/WayPoint.cs
+++ Assets/Tile/WayPoint.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
+  [SerializeField] GameObject towerPrefab;
   [SerializeField] bool isPlaceable;
   void OnMouseDown() 
   {
     if(isPlaceable)
-    Debug.Log(transform.name);
+      Instantiate(towerPrefab,transform.position, Quaternion.identity);
   }
 }
