c70dbb5c6eb926ddb226cf16f17c4e778faf3191
AndreD4 <andremai629@gmail.com>
2023-06-02 15:58:22 +0900

    Ui text

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -16,22 +16,30 @@ public class Bank : MonoBehaviour
     void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if(currentBalance < 0)
         {
           //Lose the game;
           ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+      displayBalance.text ="Gold:" + currentBalance;
     }
 
     void ReloadScene()
