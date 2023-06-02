6c5913d48de8031ab41f2018777d33efe2a7ea25
AndreD4 <andremai629@gmail.com>
2023-06-02 16:02:35 +0900

    Increasing difficulty

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
+    [SerializeField] int difficultyRamp = 1;
     int currentHitPoints = 0;
 
     Enemy enemy;
@@ -32,6 +33,7 @@ public class EnemyHealth : MonoBehaviour
         if(currentHitPoints <= 0)
         {
           gameObject.SetActive(false);
+          maxHitPoints += difficultyRamp;
           enemy.RewardGold();
         }
     }
