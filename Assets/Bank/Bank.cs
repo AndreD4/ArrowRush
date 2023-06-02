e2dc88cf3321547d364dee2dfe19dd6328ee12f4
AndreD4 <andremai629@gmail.com>
2023-06-02 15:47:55 +0900

    textmeshoro Ui display balance

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class Bank : MonoBehaviour
     
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
+
+    [SerializeField] TextMeshProUGUI displayBalance;
 
     void Awake()
     {
